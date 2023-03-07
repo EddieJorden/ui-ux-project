aa679ddbc77f9c2e5cd58033ac530a495320f59f
EddieJorden <eddie@eddiejorden.tech>
2023-03-06 18:44:36 -0800

    updated to new api, removed unused files

--- src/apiReducer.js
+++ src/apiReducer.js
@@ -3,8 +3,7 @@ const { createApi, fetchBaseQuery } = require('@reduxjs/toolkit/query/react');
 export const apiReducer = createApi({
 	reducerPath: 'apiReducer',
 	baseQuery: fetchBaseQuery({
-		baseUrl:
-			'https://eddiejorden-tech-portfolio-server-main-kposozymga-wm.a.run.app/',
+		baseUrl: 'https://portfolio-server-xa7k-bv90-main-kposozymga-wm.a.run.app/',
 	}),
 	endpoints: (builder) => ({
 		getSampleData: builder.query({
